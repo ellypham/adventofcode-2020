0af1dc53c43915de5e9fa2075384ae0f18393f66
stevebosworth <steve.bosworth@shopify.com>
2020-12-03 11:29:48 -0500

    Day 3 pairing attempt

--- day3/index.js
+++ day3/index.js
@@ -3,5 +3,31 @@ const path = require('path')
 
 let data = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8')
 
-console.log(data)
+const array = data.split('\n');
 
+let treeCount = 0;
+let position = 0;
+let moveRight = 3;
+let tree = '#';
+
+array.forEach((item, index) => {
+  if (item.length === 0){
+    return;
+  }
+
+  let newPosition = 0;
+  if (item[position] == tree) {
+    treeCount++;
+  }
+
+  newPosition = position + moveRight;
+
+  if (newPosition > item.length) {
+    position = newPosition - item.length - 1;
+  } else {
+    position = newPosition;
+  }
+})
+
+
+console.log(treeCount)
