f109055ba082e55141ed7f6066aba56701142685
stevebosworth <steve.bosworth@shopify.com>
2020-12-03 21:55:27 -0500

    Step 2

--- day3/index.js
+++ day3/index.js
@@ -5,30 +5,55 @@ let data = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8')
 
 const array = data.split('\n');
 
-let treeCount = 0;
-let position = 0;
-let moveRight = 3;
-let tree = '#';
+function countTrees(moveRight, down) {
+  let treeCount = 0;
+  let position = 0;
+  let tree = '#';
 
-array.forEach((item, index) => {
-  if (item.length === 0){
-    return;
-  }
+  array.forEach((item, index) => {
+    let newPosition = 0;
+    //skip empty lines
+    if (item.length === 0){
+      return;
+    }
 
-  let newPosition = 0;
-  if (item[position] == tree) {
-    treeCount++;
-  }
+    // skip any row when slope is two
+    if (down === 2 && isOdd(index)) {
+      // console.log(index)
+      // console.log(item)
+      // console.log(positio)
+      return;
+    }
 
-  newPosition = position + moveRight;
+    if (item[position] == tree) {
+      treeCount++;
+      // console.log(`position: ${position}`)
+      // console.log(`lineValue: ${item}`)
+      // console.log(`treecount: ${treeCount}`)
+      // console.log(`line: ${index + 1}`)
+    }
 
-  if (newPosition > (item.length - 1)) {
-    position = newPosition - item.length;
-    console.log(`reset: ${position}`)
-  } else {
-    position = newPosition;
-    console.log(`position: ${position}`)
-  }
-})
+    newPosition = position + moveRight;
 
-console.log(treeCount)
+    if (newPosition > (item.length - 1)) {
+      position = newPosition - item.length;
+    } else {
+      position = newPosition;
+    }
+  })
+  // console.log(treeCount)
+
+  return treeCount;
+}
+
+const total = countTrees(1, 1)*
+  countTrees(3, 1)*
+  countTrees(5, 1)*
+  countTrees(7, 1)*
+  countTrees(1, 2);
+
+console.log(total)
+
+
+
+function isOdd(num) { return num % 2; }
